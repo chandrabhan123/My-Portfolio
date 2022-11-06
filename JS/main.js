fd3931baa4cc6d79759926aca6023f985f5fe307
Chandrabhan Banjara <chandrabhannayak36890@gmail.com>
2022-11-06 18:57:54 +0530

    Full responsive resume section

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,12 @@
 // Header scroll
-
-
+let nav = document.querySelector(".navbar")
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add('header-scrolled');
+    }else
+    {
+        nav.classList.romove("header-scrolled");
+    }
+}
 
 // mouse hover design
